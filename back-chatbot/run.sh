769f373b75d7e0345b6aae254f0d0deca9baeef8
Ubuntu <ubuntu@ip-172-31-7-250.ap-northeast-2.compute.internal>
2025-05-06 14:14:44 +0000

    fix run.sh run background

--- back-chatbot/run.sh
+++ back-chatbot/run.sh
@@ -13,4 +13,5 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 # FastAPI 서버 실행
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
+#uvicorn main:app --reload --host 0.0.0.0 --port 8000
+nohup uvicorn main:app --host 0.0.0.0 --port 8000 &
